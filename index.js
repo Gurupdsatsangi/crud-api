8e559b6c37d963ae66c605578cfd9bf72fe620e1
gurup <guruprasadsatsangi15@gmail.com>
2025-04-13 01:26:02 +0530

    updat

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app=express();
 const path =require("path");
 app.use(express.urlencoded({extended:true}));
 
+const {v4:uuidv4}=require('uuid');
+const methodoverride=require("method-override");
 const port=8080;
 
+app.use(methodoverride('_method'))
 app.listen(port,() =>{
     console.log("listening");
 });
@@ -15,15 +18,15 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 
 let posts = [
-    {
+    {   id:uuidv4(),
         username:"apnacollege",
         content:"i love coding"
     },
-    {
+    {   id:uuidv4(),
         username:"gps",
         content:"i go to college"
     },
-    {
+    {   id:uuidv4(),
         username:"ayush",
         content:"i am a boy"
     },
@@ -38,7 +41,33 @@ app.get("/posts/new",(req,res)=> {
 })
 app.post("/posts",(req,res)=>{
     let {username,content}=req.body;
-    posts.push({username,content});
+    let id=uuidv4();
+    posts.push({id,username,content});
     console.log(req.body);
+    res.redirect("/posts");
+});
+app.get("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p) =>id===p.id);
+    res.render("show.ejs",{post});
+});
+
+app.patch("/posts/:id",(req,res)=>{
+    let{id}=req.params;
+    let newcontent=req.body.content;
+    let post =posts.find((p)=> id===p.id);
+    post.content=newcontent;
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let{id}=req.params;
+    let post =posts.find((p)=> id===p.id);
+    res.render("edit.ejs",{post});
+    
+})
+app.delete("/posts/:id",(req,res)=>{
+    let{id}=req.params;
+    posts =posts.filter((p)=> id!==p.id);
     res.redirect("/posts");
 })
